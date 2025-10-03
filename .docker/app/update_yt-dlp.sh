c8e00416791ca81fd28694607fd173dfc2e8fc94
Josip Igrec <josip@igr.ec>
2025-10-04 00:52:29 +0200

    Fix updating yt-dlp not working due to permissions

--- .docker/app/update_yt-dlp.sh
+++ .docker/app/update_yt-dlp.sh
@@ -5,7 +5,7 @@ YT_DLP_VERSION="${YT_DLP_VERSION:-latest}"
 YT_DLP_UPDATE_LOG_FILE="${YT_DLP_UPDATE_LOG_FILE:-/tmp/update_yt-dlp.log}"
 
 curl -L "https://github.com/yt-dlp/yt-dlp/releases/${YT_DLP_VERSION}/download/yt-dlp" -o '/usr/local/bin/yt-dlp' &&
-  chmod a+rx '/usr/local/bin/yt-dlp'
+	chmod a+rx,g+w '/usr/local/bin/yt-dlp'
 
 echo "[$(date -R)] yt-dlp updated to ${YT_DLP_VERSION}" >>"${YT_DLP_UPDATE_LOG_FILE}"
-chmod a+rw "${YT_DLP_UPDATE_LOG_FILE}"
+chmod a+rw,g+w "${YT_DLP_UPDATE_LOG_FILE}"
